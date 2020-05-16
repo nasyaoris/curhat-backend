79d034b9380caa73e005da6cc05bf2768e3b22c7
Ray Azrin Karim <rayazrink@gmail.com>
2020-05-16 08:22:53 +0700

    Create base model

--- master/models.py
+++ master/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.model import User
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='%(class)s_created_by')
+    updated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='%(class)s_updated_by', blank=True, null=True)
+
+    class Meta:
+        abstract = True
